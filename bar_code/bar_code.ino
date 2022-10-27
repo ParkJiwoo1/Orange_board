aa93c9a19b61e7e4da25203d7e5146b7cb39336a
Jiwoo Park <jiwoo9446@naver.com>
2022-10-27 22:16:49 +0900

    initial information

--- bar_code/bar_code.ino
+++ bar_code/bar_code.ino
@@ -1,37 +1,43 @@
 #include <SoftwareSerial.h> // 시리얼 포트가 부족한 경우, 다른 기기와 시리얼 통신을 위한 아두이노 라이브러리
 
-SoftwareSerial mySerial(2, 3); // RX, TX
+SoftwareSerial mySerial('RX1', 'TX1'); // RX, TX
 
-String str = "";    //시리얼수신 문자열 저장
-String msg="";
+String str = ""; //시리얼수신 문자열 저장
+String msg = "";
 int intStr;
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(4,OUTPUT);
+void setup()
+{
+  Serial1.begin(9600);
+  pinMode(27, OUTPUT);
   mySerial.begin(9600);
 }
 
-void loop() { 
-// available문 안에서 동시 확인
-if(mySerial.available()){
-    str = mySerial.readString();             //read()가 아닌 readString()으로 읽기 :: 문자열로 읽기
-                                             //왜? 웬만한 교과서에는 read()로 한 문자만 읽어오는 것만 설명하는지 모르겠음
-    str = str.substring(0, str.length()-2);  //시리얼모니터에서 엔터 치면서 마지막에 송신한 '\n' 제거
-    Serial.println(str);                       // 수신 데이터 확인
-    //Serial.print("--------( ");                // 구분선
-    //Serial.print(str.length());              // 수신 받은 문자열 길이 확인
-    //Serial.print(" )--------// ");              // 구분선
-   
+void loop()
+{
+  Serial1.println("test");
+  // available문 안에서 동시 확인
+  if (mySerial.available())
+  {
+    str = mySerial.readString(); // read()가 아닌 readString()으로 읽기 :: 문자열로 읽기
+                                 //왜? 웬만한 교과서에는 read()로 한 문자만 읽어오는 것만 설명하는지 모르겠음
+    str = str.substring(0, str.length() - 2); //시리얼모니터에서 엔터 치면서 마지막에 송신한 '\n' 제거
+    Serial1.println(str);                      // 수신 데이터 확인
+    // Serial.print("--------( ");                // 구분선
+    // Serial.print(str.length());              // 수신 받은 문자열 길이 확인
+    // Serial.print(" )--------// ");              // 구분선
   }
-  if(Serial.available()){
-      msg=Serial.readStringUntil('\n');
-      if (msg.indexOf("on")>-1){
-      digitalWrite(4,HIGH);
-      }
-      else if (msg.indexOf("off")>-1){
-       
-      digitalWrite(4,LOW);
-      }
+  if (Serial1.available())
+  {
+    msg = Serial1.readStringUntil('\n');
+    if (msg.indexOf("on") > -1)
+    {
+      digitalWrite(27, HIGH);
+    }
+    else if (msg.indexOf("off") > -1)
+    {
+
+      digitalWrite(27, LOW);
+    }
   }
 }
